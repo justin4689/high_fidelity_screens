3b7a3e95cd4b5a2dd88d1c88cd83af4fa391a4a0
Justin Trah <justintrah8@gmail.com>
2026-06-11 12:16:03 +0000

    register page implemented

--- lib/screens/forgot_password.dart
+++ lib/screens/forgot_password.dart
@@ -7,9 +7,156 @@ class ForgotPassword extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 70),
 
-      body: Center(
-        child: Text('Forgot Password' , style: TextStyle(color: AppColors.textColor),),
+                Center(
+                  child: Text(
+                    'Sign In',
+                    style: TextStyle(
+                      color: AppColors.primary,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 10),
+
+                Text(
+                  'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum.',
+                  style: TextStyle(color: AppColors.textColor, fontSize: 15),
+                  textAlign: TextAlign.center,
+                ),
+
+                const SizedBox(height: 20),
+
+                // SOCIAL BUTTONS
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 56,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: const Color(0xffF5F9FE),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/images/facebook.png'),
+                            const SizedBox(width: 10),
+                            const Text(
+                              'Facebook',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(width: 10),
+
+                    Expanded(
+                      child: Container(
+                        height: 56,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: const Color(0xffF5F9FE),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset('assets/images/google.png'),
+                            const SizedBox(width: 10),
+                            const Text(
+                              'Google',
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 20),
+
+                // OR DIVIDER
+                Row(
+                  children: [
+                    Expanded(child: Container(height: 1, color: Colors.grey)),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Text('Or'),
+                    ),
+                    Expanded(child: Container(height: 1, color: Colors.grey)),
+                  ],
+                ),
+
+                const SizedBox(height: 20),
+
+              
+
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color(0xffF5F9FE),
+                    hintText: 'Email / Phone Number',
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                      borderSide: BorderSide(color: AppColors.primary)
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                      borderSide: BorderSide.none
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 15),
+
+               
+
+
+                // BUTTON
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.primary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Continue',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+
+
+                // LOGIN LINK
+          
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
